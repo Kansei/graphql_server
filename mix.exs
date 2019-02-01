52bcf79c80dc6172ab68a903dd60d729889c3862
Kansei Kogami <kk7221dev@gmail.com>
2019-02-01 23:47:22 +0900

    install absinthe

--- mix.exs
+++ mix.exs
@@ -37,7 +37,9 @@ defmodule GraphqlServer.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:absinthe, "~> 1.4"},
+     {:absinthe_plug, "~> 1.4"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
